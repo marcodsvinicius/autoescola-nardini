b2f8bb4b3a4028df554f273807c2d03d85dbb73c
Marco Vinicius <marcodsvinicius@gmail.com>
2021-03-01 21:04:21 -0300

    aula 57
    incrementado um monte de parada loca, desde o globalstyle até o breakpoint.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,62 @@
+import React from 'react'
+import GlobalStyle from "../src/components/GlobalStyle";
+import { addDecorator, addParameters } from "@storybook/react";
+
+addDecorator(storyFn => (
+  <>
+    <GlobalStyle/>
+    {storyFn()}
+  </>
+));
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+const viewports = {
+  extraSmall: {
+    name: "Portrait Phone (default)",
+    styles: {
+      width: "360px",
+      height: "640px",
+    },
+  },
+  small: {
+    name: "Landscape Phone (sm)",
+    styles: {
+    width: "640px",
+    height: "360px",
+  },
+},
+
+  medium: {
+    name: "Tablet (md)",
+    styles: {
+      width: "768px",
+      height: "1024px",
+    },
+  },
+
+  large: {
+    name: "Desktop (lg)",
+    styles: {
+      width: "1024px",
+      height: "1366px",
+    },
+  },
+
+  extraLarge: {
+    name: "Large Desktop (xl)",
+    styles: {
+      width: "1280px",
+      height: "800px",
+    },
+  },
+
+};
+
+addParameters({
+  viewport: {
+    viewports
+  }
+});
---
+++ src/stories/Hero.stories.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import Hero from "../components/Hero";
+import BgHighwayImage from "./assets/carro.jpg";
+import BgCarImage from "./assets/carro2.jpg";
+import GlobalStyle from "../components/GlobalStyle"
+
+export default {
+  title: "Hero",
+  component: Hero,
+}
+
+export const usage = () => 
+<Hero title="Ganhe sua liberdade para ir e vir" image={BgHighwayImage}>
+  <p>A auto escola lider em aprovação.</p>
+</Hero>
+
+export const withList = () => (
+  <Hero title="Ganhe sua liberdade para ir e vir" image={BgCarImage}>
+  <ul>
+    <li>Lorem ipsum dolor sit amet consect.</li>
+    <li>Lorem ipsum dolor sit amet consect.</li>
+    <li>Lorem ipsum dolor sit amet consect.</li>
+  </ul>
+</Hero>
+);
