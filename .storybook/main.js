02ff0d1766fbb5fc41a8dec0c10c645d46ebae64
Marco Vinicius <marcodsvinicius@gmail.com>
2021-03-02 11:57:12 -0300

    aula 68
    update on main.js, because is a change necessary for the project according by teacher.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,16 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
+  stories: ["../src/stories/**/*.stories.js", "../src/styles/**/*.stories.js"],
+  addons: [
     "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
     "@storybook/addon-viewport",
-    "@storybook/addon-docs",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true,
+      },
+    },
     "@storybook/addon-knobs",
-  ]
-}
+  ],
+};
